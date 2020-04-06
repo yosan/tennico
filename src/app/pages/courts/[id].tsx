047729396347f6bebe8ac934d0cd8b79bf355b29
Yosuke Takahashi <taka.yosuke@gmail.com>
2020-04-07 04:39:44 +0900

    Add Head

--- src/app/pages/courts/[id].tsx
+++ src/app/pages/courts/[id].tsx
@@ -1,20 +1,45 @@
-import { NextPage } from 'next'
+import { NextPage, GetServerSideProps } from 'next'
+import Head from 'next/head'
 import * as React from 'react'
 import App from '../../components/App'
 import CourtDetails from '../../components/courts/CourtDetails'
 import Navbar from '../../components/Navbar'
 import { useRouter } from 'next/router'
+import fetch from 'node-fetch'
 
-const Court: NextPage<{}> = () => {
+interface Props {
+  name: string
+  address: string
+}
+
+const Court: NextPage<Props> = (props) => {
   const router = useRouter()
   const { id } = router.query
 
   return (
     <App>
+      <Head>
+        <title>{`${props.name} - Tennico`}</title>
+        <meta name="description" content={props.address} />
+      </Head>
       <Navbar />
       <CourtDetails id={id as string} />
     </App>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch(
+    `https://firestore.googleapis.com/v1/projects/tennico-f93a4/databases/(default)/documents/courts/${id}`
+  )
+  const data = await res.json()
+  return {
+    props: {
+      name: data.fields.name.stringValue,
+      address: data.fields.address.stringValue,
+    },
+  }
 }
 
 export default Court
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -3,9 +3,14 @@ import * as React from 'react'
 import App from '../components/App'
 import Home from '../components/Home'
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
 
 const IndexPage: NextPage<{}> = () => (
   <App>
+    <Head>
+      <title>Tennico</title>
+      <meta name="description" content="近くのテニスコートを探そう" />
+    </Head>
     <Navbar />
     <Home />
   </App>
