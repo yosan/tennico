668758e276f67a4d1999ee0a9c8afa9027373503
Yosuke Takahashi <taka.yosuke@gmail.com>
2020-04-18 03:33:33 +0900

    Add lang

---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
