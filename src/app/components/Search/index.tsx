7f6bd120f1f48dd6ae84fb1e662c7005f2168af9
Yosuke Takahashi <taka.yosuke@gmail.com>
2020-05-04 07:09:49 +0900

    Fit map bounds

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -3,6 +3,7 @@ import { FC, useCallback, useEffect, useMemo, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import GoogleMapReact from 'google-map-react'
+import { fitBounds } from 'google-map-react/utils'
 import Court from '../../models/court'
 import Pin from '../Pin'
 import { search } from '../../models/search'
@@ -43,6 +44,39 @@ const Search: FC<{}> = () => {
     )
   }, [courts])
 
+  const { center, zoom } = useMemo(() => {
+    if (!courts) {
+      return {}
+    }
+
+    if (courts.length === 1) {
+      return {
+        center: { lat: courts[0].geo.latitude, lng: courts[0].geo.longitude },
+        zoom: 16,
+      }
+    }
+
+    const lats = courts.map((court) => court.geo.latitude)
+    const lngs = courts.map((court) => court.geo.longitude)
+    const bounds = {
+      nw: {
+        lat: Math.max(...lats),
+        lng: Math.min(...lngs),
+      },
+      se: {
+        lat: Math.min(...lats),
+        lng: Math.max(...lngs),
+      },
+    }
+
+    const size = {
+      width: window.innerWidth || document.body.clientWidth,
+      height: window.innerHeight || document.body.clientHeight,
+    }
+
+    return fitBounds(bounds, size)
+  }, [courts])
+
   const onClick = useCallback((id: string) => setSelectedID(id), [])
 
   return (
@@ -53,11 +87,8 @@ const Search: FC<{}> = () => {
             bootstrapURLKeys={{
               key: google.apiKey,
             }}
-            defaultCenter={{
-              lat: courtsCenter.latitude,
-              lng: courtsCenter.longitude,
-            }}
-            defaultZoom={10}
+            defaultCenter={center}
+            defaultZoom={zoom}
           >
             {courts.map((court) => {
               return (
