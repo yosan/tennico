59a8bab0b22b6cc33dbbd70222a4edf676eb5a75
Yosuke Takahashi <taka.yosuke@gmail.com>
2021-01-22 05:04:56 +0900

    Setup all text inputs

--- src/app/components/NewCourt/index.tsx
+++ src/app/components/NewCourt/index.tsx
@@ -2,16 +2,28 @@ import { useFormik } from 'formik'
 import * as React from 'react'
 import * as Yup from 'yup'
 
+const CourtSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('必須項目です')
+    .max(100, '最大100文字で入力してください'),
+  address: Yup.string()
+    .required('必須項目です')
+    .max(100, '最大100文字で入力してください'),
+  price: Yup.string()
+    .required('必須項目です')
+    .max(200, '最大200文字で入力してください'),
+  url: Yup.string().url().max(500, '最大500文字で入力してください'),
+})
+
 const NewCourt: React.FC<Record<string, unknown>> = () => {
   const formik = useFormik({
     initialValues: {
       name: '',
+      address: '',
+      price: '',
+      url: '',
     },
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .required('必須項目です')
-        .max(100, '最大100文字で入力してください'),
-    }),
+    validationSchema: CourtSchema,
     onSubmit: (values, { setSubmitting }) => {
       alert(JSON.stringify(values, null, 2))
       setSubmitting(false)
@@ -20,7 +32,7 @@ const NewCourt: React.FC<Record<string, unknown>> = () => {
 
   return (
     <main>
-      <h2>コート追加</h2>
+      <h4>コート追加</h4>
       <form onSubmit={formik.handleSubmit}>
         <div className="row">
           <div className="input-field col s12">
@@ -28,12 +40,44 @@ const NewCourt: React.FC<Record<string, unknown>> = () => {
               className={`${formik.errors.name ? 'invalid' : 'valid'}`}
               type="text"
               id="name"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
               value={formik.values.name}
             />
             <label htmlFor="name">コート名</label>
             <span className="helper-text" data-error={formik.errors.name} />
+          </div>
+          <div className="input-field col s12">
+            <input
+              className={`${formik.errors.address ? 'invalid' : 'valid'}`}
+              type="text"
+              id="address"
+              value={formik.values.address}
+            />
+            <label htmlFor="address">住所</label>
+            <span className="helper-text" data-error={formik.errors.address} />
+          </div>
+          <div className="input-field col s12">
+            <input
+              className={`${formik.errors.price ? 'invalid' : 'valid'}`}
+              type="text"
+              id="price"
+              value={formik.values.price}
+            />
+            <label htmlFor="price">利用料金</label>
+            <span className="helper-text" data-error={formik.errors.price}>
+              {
+                '例) 1時間　平日1,300円　土日祝日1,300円　【夜間照明料】 1時間以内500円'
+              }
+            </span>
+          </div>
+          <div className="input-field col s12">
+            <input
+              className={`${formik.errors.url ? 'invalid' : 'valid'}`}
+              type="text"
+              id="url"
+              value={formik.values.url}
+            />
+            <label htmlFor="url">URL</label>
+            <span className="helper-text" data-error={formik.errors.url} />
           </div>
         </div>
         <div className="row">
