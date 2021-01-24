6ca2beb00b8713e4d090801c571fe7c9e48abceb
Yosuke Takahashi <taka.yosuke@gmail.com>
2021-01-25 05:21:49 +0900

    Modify court details style

--- src/app/components/CourtDetails/CourtDetailsTable/index.tsx
+++ src/app/components/CourtDetails/CourtDetailsTable/index.tsx
@@ -1,9 +1,8 @@
-import Paper from '@material-ui/core/Paper'
+import Link from '@material-ui/core/Link'
 import { makeStyles } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
 import TableRow from '@material-ui/core/TableRow'
 import { Court } from 'models/court'
 import { SurfaceType, surfaceTypeName } from 'models/surfaceType'
@@ -15,8 +14,12 @@ interface Props {
 }
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+  labelCell: {
+    wordBreak: 'keep-all',
+  },
+  valueCell: {
+    overflowWrap: 'break-word',
+    wordWrap: 'break-word',
   },
 })
 
@@ -24,58 +27,67 @@ const CourtDetailsTable: React.FC<Props> = ({ court }) => {
   const classes = useStyles()
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableBody>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              住所
-            </TableCell>
-            <TableCell>{court.address}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              料金
-            </TableCell>
-            <TableCell>{court.price}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              面数
-            </TableCell>
-            <TableCell>
-              {court.surfaces &&
-                Object.keys(court.surfaces).map((type, index) => {
-                  return (
-                    <p key={index}>
-                      {surfaceTypeName(SurfaceType[type])}{' '}
-                      {court.surfaces[type]}面
-                    </p>
-                  )
-                })}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              ナイター
-            </TableCell>
-            <TableCell>{court.nighter ? 'あり' : 'なし'}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              URL
-            </TableCell>
-            <TableCell>{court.url}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              追加日
-            </TableCell>
-            <TableCell>{moment(court.createdAt.toDate()).calendar()}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell className={classes.labelCell} component="th" scope="row">
+            住所
+          </TableCell>
+          <TableCell className={classes.valueCell}>{court.address}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className={classes.labelCell} component="th" scope="row">
+            料金
+          </TableCell>
+          <TableCell className={classes.valueCell}>{court.price}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className={classes.labelCell} component="th" scope="row">
+            面数
+          </TableCell>
+          <TableCell className={classes.valueCell}>
+            {court.surfaces &&
+              Object.keys(court.surfaces).map((type, index) => {
+                return (
+                  <p key={index}>
+                    {surfaceTypeName(SurfaceType[type])} {court.surfaces[type]}
+                    面
+                  </p>
+                )
+              })}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className={classes.labelCell} component="th" scope="row">
+            ナイター
+          </TableCell>
+          <TableCell className={classes.valueCell}>
+            {court.nighter ? 'あり' : 'なし'}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell component="th" scope="row">
+            URL
+          </TableCell>
+          <TableCell
+            className={classes.valueCell}
+            style={{ overflowWrap: 'anywhere' }}
+          >
+            <Link href={court.url} target="_blank">
+              {court.url}
+            </Link>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className={classes.labelCell} component="th" scope="row">
+            追加日
+          </TableCell>
+          <TableCell className={classes.valueCell}>
+            {moment(court.createdAt.toDate()).calendar()}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   )
 }
 
