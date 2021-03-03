cdb652994a3937a405eb1849df7efc63e0ccf723
Yosuke Takahashi <taka.yosuke@gmail.com>
2021-03-04 06:07:25 +0900

    Add dummy env values

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,22 +1,22 @@
 const config = {
   firebase: {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ?? '',
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? 'xxx',
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? 'xxx',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? 'xxx',
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ?? 'xxx',
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? 'xxx',
     messagingSenderId:
-      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '',
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? '',
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET ?? '',
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? 'xxx',
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'xxx',
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET ?? 'xxx',
   },
   algolia: {
-    appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? '',
-    apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY ?? '',
+    appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? 'xxx',
+    apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY ?? 'xxx',
   },
   google: {
-    apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? '',
-    apiKeyGeo: process.env.NEXT_PUBLIC_GOOGLE_API_KEY_GEO ?? '',
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? 'xxx',
+    apiKeyGeo: process.env.NEXT_PUBLIC_GOOGLE_API_KEY_GEO ?? 'xxx',
   },
 }
 
