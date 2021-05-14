2a5cf39094502a68ab43201c86de0902162bdfbf
Yosuke Takahashi <taka.yosuke@gmail.com>
2021-05-15 01:35:11 +0900

    Do not fit when search mode is text

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -77,7 +77,7 @@ const Home: FC<Record<string, unknown>> = () => {
   }, [selectedID, courtDocs])
 
   useEffect(() => {
-    if (map) {
+    if (map && mode === 'text') {
       const bounds = new (window as any).google.maps.LatLngBounds()
 
       courtDocs.forEach((courtDoc) => {
