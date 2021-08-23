d280242c089c5327a086df99a282a7bd7c9d8653
Yosuke Takahashi <taka.yosuke@gmail.com>
2021-08-24 05:22:38 +0900

    Lint

--- src/app/hooks/index.tsx
+++ src/app/hooks/index.tsx
@@ -1,6 +1,9 @@
+import { AnyAction, Dispatch, ThunkDispatch } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { AppDispatch, RootState } from 'store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = (): ThunkDispatch<unknown, null, AnyAction> &
+  ThunkDispatch<unknown, undefined, AnyAction> &
+  Dispatch<AnyAction> => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
